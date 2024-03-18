1d3fdaee7a33ad8be0c4d20d4975d35c190c7ed5
rubensalban <rubensalban@gmail.com>
2024-03-18 15:49:26 +0100

    Configuration fichier de ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps : [{
       name: 'CASINO-SUPERMARKET',
       script: 'app.js',
-      instances: 4,
+      instances: 2,
       watch: '.',
       ignore_watch: ['node_modules', 'public'],
     }, {
@@ -15,7 +15,7 @@ module.exports = {
         host : '194.163.180.27',
         ref  : 'origin/main',
         repo : 'https://github.com/NYOTA-PROJECTS/CASINO-API-ONLINE.git',
-        path : '/root/api-casino',
+        path : '/root/api-casinomarket',
         'pre-deploy-local': '',
         'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
         'pre-setup': ''
